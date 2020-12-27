e027d428d7c121e20eb8894f9f8fcd809683d845
auhide <bo0opr@gmail.com>
2020-12-27 15:14:52 +0200

    Added Cross Origin Resource Sharing to the API

--- api.py
+++ api.py
@@ -2,12 +2,14 @@ import json
 
 import numpy as np
 from flask import Flask, request
+from flask_cors import CORS
 from flask_restful import Resource, Api
 
 from resources.resource import Architecture
 
 
 app = Flask(__name__)
+cors = CORS(app)
 api = Api(app)
 
 api.add_resource(Architecture, "/architecture")
--- start_api.sh
+++ start_api.sh
@@ -4,4 +4,4 @@ source venv/bin/activate
 echo "Starting the Flask API"
 export FLASK_APP=api.py
 export FLASK_ENV=development
-flask run
+flask run --host=0.0.0.0
